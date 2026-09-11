fd8907b9e7b4df1e036c10962af6baec3e416bd4
izakh13 <ihor.zkhrnko@gmail.com>
2026-09-11 07:52:54 +0000

    Add API for tasks

--- config/urls.py
+++ config/urls.py
@@ -14,10 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from rest_framework.routers import DefaultRouter
 from django.contrib import admin
 from django.urls import path, include
+from tasks.views import TaskViewSet
+
+router = DefaultRouter()
+router.register('api/tasks', TaskViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tasks/', include('tasks.urls'))
+    path('tasks/', include('tasks.urls')),
+    path('', include(router.urls)),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,13 @@
+from rest_framework import viewsets
 from django.shortcuts import render, redirect
 from .models import Task
 from .forms import TaskForm
 from .services import improve_task_description
+from .serializers import TaskSerializer
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
 
 def task_list(request):
     tasks = Task.objects.all()
