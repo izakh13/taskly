9879ff7376a04c9ea4b9581419f87c96477e6f39
izakh13 <ihor.zkhrnko@gmail.com>
2026-09-13 16:48:24 +0000

    Add page for redirection after logout

--- config/urls.py
+++ config/urls.py
@@ -28,6 +28,6 @@ urlpatterns = [
     path('tasks/', include('tasks.urls')),
     path('', include(router.urls)),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('accounts/register/', register, name='register')
 ]
